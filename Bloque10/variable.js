600d6bfb1ced49e6471068ed2f7e614b83e28b75
Gonzalo Heredia <gonzalo.heredia.gh8@gmail.com>
2023-01-17 21:44:04 -0300

    Introducción a variables en JavaScript

---
+++ Bloque10/variable.js
@@ -0,0 +1,11 @@
+
+let length = 16;                                //Número
+let lastName = "Jonhson";                       //String: Cadena de texto
+let x ={nombre: "Jonh", apellido: "Doe"}        //Objeto
+
+
+//Tipos de variables
+
+let z;              //Se define la variable z y esta "undefined"
+z = 5;              //Ahora z es un numero
+z = "Jonh"          //Ahora z es un string
